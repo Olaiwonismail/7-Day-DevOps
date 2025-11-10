146cf66856007319aa2dec64150f360ff68c3735
-g <-g>
2025-11-10 20:11:21 +0100

    docs: fixed typo in readme file

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ async def get_rick_and_morty_character():
     import random
     random_id = random.randint(1, 100)
     image_url = f'https://rickandmortyapi.com/api/character/avatar/{random_id}.jpeg'
+    
     img_data = requests.get(image_url).content 
     return Response(content=img_data, media_type="image/jpeg")
 
